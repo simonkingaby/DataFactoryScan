94bbad757f65591c74266053612b769e57787e3b
Kingaby <skingaby@bmi.com>
2019-01-29 07:35:28 -0600

    Add project files.

---
+++ DataFactoryScan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataFactoryScan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
